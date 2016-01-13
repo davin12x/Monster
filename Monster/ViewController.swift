999d45f75b473ead3e9d924fa654333f5d397f4c
Lalit Bagga <Davin12x@Lalits-MacBook-Pro.local>
2016-01-12 21:40:01 -0500

    Animation Added

--- Monster/ViewController.swift
+++ Monster/ViewController.swift
@@ -9,13 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Monster: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-           }
-
-  
-
-
+        var imageArray = [UIImage]()
+        for var x = 1; x <= 4; x++ {
+            let img = UIImage(named: "idle\(x).png")
+            imageArray.append(img!)
+        }
+        Monster.animationImages = imageArray
+        Monster.animationDuration = 0.8
+        Monster.animationRepeatCount = 0
+        Monster.startAnimating()
+        
+    }
 }
 
